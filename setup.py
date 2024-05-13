324498aa13f027c5bd2f3055839f6ee27044302b
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-05-13 14:48:45 +0100

    project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Text-Summarization-Pytorch",
+    version="0.0.1",
+    author="Dhruv Saxena,
+    author_email="saxena25dhruv@gmail.com",
+    packages=find_packages(),
+)
